0fcca4f0ade9b90afad530472c30ee730d732c22
Cameron <cmclaren@tulane.edu>
2023-10-25 23:40:50 -0500

    Implementation & Questions

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Reciation 5
 ## Answers
 
-**Name:**_________________________
+**Name:** Cameron McLaren
 
 
 Place all written answers from `recitation-05.md` here for easier grading.
@@ -14,7 +14,10 @@ Place all written answers from `recitation-05.md` here for easier grading.
 
 - **1b.**
 
-
+Fixed qsort implementation seems to perform better with the highest n values at n=100000 as well as the other high values, with the exception of n=50000. The difference at the highest level especially is the most drastic. At the lowest levels from n=100 to n=1000, the values were more similar and the random sort actually took less time, but on a much smaller scale. This relates to the asymptotic bounds because the worst case for quicksorting with a random pivot is O(n^2) and the worst complexity of a fixed pivot is O(nlogn). This is in line with the values which were also plotted on the graph above, with the red dots being the data from a fixed pivot and the blue being from the random pivot. The fixed pivot and random pivot both curve sharply upward, but the random takes on a larger curve than the fixed pivot which is in line with their worst case time complexities. It is clear that as the list grows exponentially, so too does the time it takes to sort, which is what we would expect from the time complexities.
 
 
 - **1c.**
+Timsort is the most efficient. Timsort has an asymptotic runtime of O(nlogn), which is faster than the runtimes of quicksort has an asymptotic runtime of O(n^2)
+
+![Alt text](image.png)
--- main.py
+++ main.py
@@ -4,7 +4,36 @@ import tabulate
 
 def qsort(a, pivot_fn):
     ## TO DO
-    pass
+    if len(a) <= 1:
+        return a
+    else:
+        pivot = pivot_fn(a)
+        
+        left = list(filter(lambda x: x < pivot, a))
+        center = list(filter(lambda x: x == pivot, a))
+        right = list(filter(lambda x: x > pivot, a))
+        return qsort(left, pivot_fn) + center + qsort(right, pivot_fn)
+
+def fixed_pivot(a):
+    return a[0]
+
+def random_pivot(a):
+    return random.choice(a)
+
+# def ssort(L):
+#     if (len(L) == 1):
+#         return(L)
+#     else:
+#         m = L.index(min(L))
+#         L[0], L[m] = L[m], L[0]
+#         return ssort(L[1:])
+
+def ssort(L):
+    for i in range(len(L)):
+        print(L)
+        m = L.index(min(L[i:]))
+        L[i], L[m] = L[m], L[i]
+    return L
     
 def time_search(sort_fn, mylist):
     """
@@ -40,27 +69,29 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
       for each method to run on each value of n
     """
     ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
+    qsort_fixed_pivot = lambda a: qsort(a, fixed_pivot) 
+    qsort_random_pivot = lambda a: qsort(a, random_pivot)
+    tim_sort = sorted
     result = []
     for size in sizes:
         # create list in ascending order
         mylist = list(range(size))
         # shuffles list if needed
-        #random.shuffle(mylist)
+        random.shuffle(mylist)
         result.append([
             len(mylist),
             time_search(qsort_fixed_pivot, mylist),
             time_search(qsort_random_pivot, mylist),
+            time_search(ssort, mylist),
+            time_search(tim_sort, mylist),
         ])
     return result
-    ###
+
 
 def print_results(results):
     """ change as needed for comparisons """
     print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
+                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot', 's-sort', 'tim-sort'],
                             floatfmt=".3f",
                             tablefmt="github"))
 
